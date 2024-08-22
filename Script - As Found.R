23c3d66ad981ccb10407e1ae1e2029b85f374d31
Guilherme Araujo Santos <guilhermee.guilherme@hotmail.com>
2024-08-22 11:06:33 -0300

    English Sub

--- Script - As Found.R
+++ Script - As Found.R
@@ -1,31 +1,37 @@
-# INSTALAÇÃO BIBLIOTECAS
+####################################################################################################
+################################### LIBRARIES INSTALLATION #########################################
+####################################################################################################
 
-library("readxl")    # LER EXCEL
-library("dplyr")     # MANIPULAR DATAFRAME
-library("ggplot2")   # PLOTAR GRAFICOS
-library("viridis")   # PALETA DE CORES VIRIDIS
-library('gganimate') # CRIAR GIF
-library('writexl')   # SALVAR EXCEL
+library("readxl")    # READ EXCEL
+library("dplyr")     # MANIPULATE DATAFRAME
+library("ggplot2")   # FOR PLOTTING
+library("viridis")   # VIRIDIS COLOR PALETTE
+library('gganimate') # CREATE GIF
+library('writexl')   # SAVE EXCEL
 library('akima')
 
-########## IMPORTANDO MAPEAMENTOS (50,200,300 mm)
+####################################################################################################
+################################### DATABASE TRATAMENT #############################################
+####################################################################################################
+#### Importing, combining and identifying the results of each mapping.
 
 map_50 = read_excel("Mapeamento  As Found - 50mm.xlsx") 
 map_200 = read_excel("Mapeamento  As Found - 200mm.xlsx")
 map_300 = read_excel("Mapeamento  As Found - 300mm.xlsx")
 
-########### COMBINANDO E IDENTIFICANDO MAPEAMENTOS 
 data <- bind_rows(
   map_50 %>% mutate(Y = 50),
   map_200 %>% mutate(Y = 200),
   map_300 %>% mutate(Y = 300)
 )
 
-# CRIANDO FUNÇÃO DE INTERPOLAÇÃO
-# Ele vai pegar uma base de dados(data), os pontos a serem interpolados
-# (y1 e y2), e o novo ponto a ser encontrado,
-# então selecionará todos os valores para as mesmas coordenadas y1 e y2
-# então realiza a interpolação
+####################################################################################################
+############################### INTEPOLATE FUNCTION ################################################
+####################################################################################################
+# DATA = DATAFRAME
+# Y1 = BEGIN OF INTEPORLATE
+# Y2 = END OF INTEPORLATE
+# NEW_Y = DESIRED POINT 
 
 
 interpolate_y <- function(data, y1, y2, new_y) {
@@ -46,35 +52,35 @@ interpolate_y <- function(data, y1, y2, new_y) {
 
 
 
-############# INTERPOLANDO PARA Y = 100,150,250
+####################################################################################################
+###############################    IINTERPOLAZING   ################################################
+####################################################################################################
+# INTERPOLAZING, MATCHING AND SAVING DATA
 
 new_data_100 <- interpolate_y(data, 50, 200, 100)
 new_data_150 <- interpolate_y(data, 50, 200, 150)
 new_data_250 <- interpolate_y(data, 200, 300, 250)
 
-############# CRIANDO BASE DE DADOS E SALVANDO 
-
 as_found_final_data <- bind_rows(data, new_data_100, new_data_150,new_data_250)
 write_xlsx(as_found_final_data, "as_found_final_data.xlsx")
 
 
-############################ PLOTANDO GRAFICOS ################################
+####################################################################################################
+###############################    PLOT LOOP  ######################################################
+####################################################################################################
+# LOOP PLOT FOR EACH DISTANCE(50,100,150,200,250,300mm)
+
+unique_Y <- unique(as_found_final_data$Y) 
 
 
 
-unique_Y <- unique(as_found_final_data$Y) # Obtenha os valores únicos de Y
-
-
-# Loop através de cada valor único de Y
 
 for (y_value in unique_Y) { 
   
-  # Filtrar o data frame pelo valor atual de Y
-  
+   
   filtered_data <- as_found_final_data %>% filter(Y == y_value)
   
-  # Criar o gráfico
-  
+   
   plot_pa <- ggplot(filtered_data, aes(x = X, y = Z, fill = Pa)) +
     geom_raster(aes(fill = Pa), interpolate = TRUE) +
     scale_fill_viridis_c() +
@@ -86,7 +92,7 @@ for (y_value in unique_Y) {
                        labels = function(y) paste0(y, "mm"))+
     ggtitle(paste("Mapeamento as Found- ", y_value, "mm"))
 
-  # Salvar o gráfico
+ 
   ggsave(filename = paste0("Mapeamento as Found_", y_value,"mm" ,".png"), 
          plot = plot_pa,
          width = 10, 
@@ -97,7 +103,11 @@ for (y_value in unique_Y) {
   }
 
 
-######################## PLOTANDO GIF MAPA DE CALOR ##############################
+####################################################################################################
+###############################    HEATMAP GIT   #################################################
+####################################################################################################
+# PLOTTING AND SAVING HEATMAP GIF
+                      
 
 p <- ggplot(as_found_final_data, aes(x = X, y = Z, fill = Pa)) +
   geom_raster(aes(fill = Pa), interpolate = TRUE) +
@@ -112,7 +122,5 @@ p <- ggplot(as_found_final_data, aes(x = X, y = Z, fill = Pa)) +
   scale_y_continuous(breaks = seq(50,500,by =50),
                      labels = function(y) paste0(y, "mm"))+
   theme_bw()
-
-
-############### SALVANDO GIF
+                     
 anim_save("Heatmap as Found.gif", animation = p)
